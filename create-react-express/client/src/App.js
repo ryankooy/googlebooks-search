91de29c9b2ed206e0ef8a2d8efe8fd155e33fbef
Ryan Kooy <rwkooy@gmail.com>
2019-11-11 11:45:43 -0500

    added mongoose, socket-io, mvc folders

---
+++ create-react-express/client/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import { subscribeToTimer } from './api';
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    subscribeToTimer((err, timestamp) => this.setState({ 
+      timestamp 
+    }));
+  }
+
+  state = {
+    timestamp: 'no timestamp yet'
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Here's a timer for this Google Books Search app.</h2>
+        </div>
+        <p className="App-intro">
+          This is the timer value: {this.state.timestamp}.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ create-react-express/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const path = require("path");
+const io = require('socket.io')();
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+io.on('connection', (client) => {
+  client.on('subscribeToTimer', (interval) => {
+    console.log('client is subscribing to timer with interval ', interval);
+    setInterval(() => {
+      client.emit('timer', new Date());
+    }, interval);
+  });
+});
+
+const port = 8000;
+io.listen(port);
+console.log('listening on port ', port);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
