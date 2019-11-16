315b7b43d6de84e1ff88d85dbfee3bc7231d36b6
Ryan Kooy <rwkooy@gmail.com>
2019-11-15 21:38:09 -0500

    heroku deployment

--- create-react-express/server/server.js
+++ create-react-express/server/server.js
@@ -14,7 +14,6 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 
-
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/googlebooks';
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
