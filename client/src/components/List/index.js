41d3d80a52f09f20634697753f397f770eac0280
Ryan Kooy <48595828+ryankooy@users.noreply.github.com>
2019-11-18 19:32:22 -0500

    Update index.js

--- client/src/components/List/index.js
+++ client/src/components/List/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Button, List, Image, Segment, Grid, Header } from 'semantic-ui-react';
-// import Thumbnail from '../Thumbnail';
+import  from '../Thumbnail';
 import './style.css';
 import API from '../../utils/API';
 
@@ -14,29 +14,13 @@ export function BookList(props) {
   );
 }
 
-function save(title, authors, description, image, link) {
-  const book = {
-    title: title,
-    authors: authors,
-    description: description,
-    image: image,
-    link: link
-  }
-  
-  API.saveBooks(book)
-    .then(dbBook => {
-      console.log(dbBook);
-    })
-    .catch(err => console.log(err));
-}
-
 export function ListItem({
   image,
   title,
   authors,
   description,
   link
-}) {
+}, { children }) {
   return (
     <Segment raised placeholder>
       <Grid columns={1}>
@@ -48,9 +32,7 @@ export function ListItem({
         </Grid.Column>
         <Grid.Column>
           <Button.Group size='big' fluid={true}>
-            <Button href={link} circular={true} color='olive'>View</Button>
-            <Button.Or />
-            <Button onClick={() => save(title, authors, description, image, link)} color='orange'>Save</Button>
+            { children }
           </Button.Group>
           <Segment>
             {description}
