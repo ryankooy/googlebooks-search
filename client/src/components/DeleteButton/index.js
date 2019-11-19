f11720327dc6b0d4dbd05b8db75da876451e532b
Ryan Kooy <48595828+ryankooy@users.noreply.github.com>
2019-11-18 19:34:01 -0500

    Update index.js

--- client/src/components/DeleteButton/index.js
+++ client/src/components/DeleteButton/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import {  } from 'semantic-ui-react';
+import { Header, List, Container} from 'semantic-ui-react';
+import API from '../utils/API';
 import './style.css';
+
+function delete(id) {
+  const thisBook = { _id: id};
+  
+  API.deleteBook(thisBook)
+    .then(res => this.loadBooks())
+    .catch(err => console.log(err));
+}
 
 function DeleteButton(props) {
   return (
-    <span className='' {...props} role="button" tabIndex="0">
-      ✗
+    <span>
+      <Button {...props} onClick={() => delete(this._id)} color='red'>✗</Button>
     </span>
   );
 };
